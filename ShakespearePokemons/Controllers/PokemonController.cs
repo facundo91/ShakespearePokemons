a3caf4ec430eea6f91ca009a8515c3b9bffebc9b
Facundo Ezequiel Aita <facundo91@gmail.com>
2020-12-04 10:46:02 -0300

    Adding Pokemon Controller

---
+++ ShakespearePokemons/Controllers/PokemonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ShakespearePokemons.Contracts;
+using ShakespearePokemons.Contracts.Response;
+using System.Threading.Tasks;
+using static Microsoft.AspNetCore.Http.StatusCodes;
+using static ShakespearePokemons.Contracts.ApiVersions;
+
+namespace ShakespearePokemons.Controllers
+{
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ApiVersion(V1Tag)]
+    [ProducesResponseType(typeof(PokemonResponse), Status200OK)]
+    [ProducesResponseType(Status400BadRequest)]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet(ApiRoutes.Pokemon.GetTranslation)]
+        public async Task<IActionResult> GetTranslation(
+            [FromRoute] string pokemonName, 
+            [FromServices] IOptions<ApiBehaviorOptions> apiBehaviorOptions)
+        {
+            if (string.IsNullOrWhiteSpace(pokemonName))
+            {
+                ModelState.AddModelError("Pokemon Name", "Pokemon's name cannot be null, empty nor white space.");
+                return apiBehaviorOptions.Value.InvalidModelStateResponseFactory(ControllerContext);
+            }
+
+            return Ok(new PokemonResponse(pokemonName, "Fake description"));
+        }
+    }
+}
